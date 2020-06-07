ce280044504b85389d28e1131b01df31b336093e
kss682 <kss682@gmail.com>
2020-06-07 15:56:20 +0530

    data written locally

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,19 @@
-`use strict`
+`use strict`;
+var fs = require('fs');
 var task_id = 0;
+var task_buffer = {};
+
+
+function update_data_json(){
+  fs.writeFile('data.json',JSON.stringify(task_buffer), function(err){
+    if (err)
+      console.log(err);
+    else
+      console.log("task updated");
+  });
+
+}
+
 
 function choose_color(priority_value){
   var bgc = ""
@@ -18,18 +32,21 @@ function choose_color(priority_value){
 function delete_task(trash_className){
   trash_can = document.getElementsByClassName(trash_className)[0];
   task_div = trash_can.parentNode;
+  delete task_buffer[task_div.id];
   task_div.remove();
+  update_data_json();
 }
 
 
 function create_task(){
   task_id = task_id + 1;
-
+  
   task = document.getElementById("input").value
   priority = document.getElementById("priority");
   priority_value = priority.options[priority.selectedIndex].value;
   color = choose_color(priority_value);
   console.log(color);
+
   // creating new div for task
   new_div = document.createElement("div");
   //new_div.appendChild(trash_can);
@@ -45,8 +62,27 @@ function create_task(){
   trash_can.setAttribute("onclick","delete_task(this.className)")
   
   document.getElementById(task_id).appendChild(trash_can);
+
+  // adding the task and priority to task buffer
+  key = String(task_id)
+  task_buffer[key] = {};
+  task_buffer[key]["task_name"] = task;
+  task_buffer[key]["priority"] = priority_value;
+  
+  // writing task buffer to data.json
+  update_data_json();
 }
 
+
+function initialize_window(){
+  fs.readFile('data.json', function(err, task_buffer){
+      if(err)
+        console.log(err);
+  });
+  for(key in task_buffer){
+    console.log(key);
+  }
+}
 
 function addtask(){
   modal = document.getElementsByClassName("modal")[0];
